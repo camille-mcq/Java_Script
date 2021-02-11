dbfc20bde939aed8c9321ac39ac27b58b30e8784
Camille Marcq <camillemarcq@MacBook-Pro-de-Camille.local>
2021-02-11 16:54:45 +0100

    DOM_Ex

--- Revision Ex_05/array.js
+++ Revision Ex_05/array.js
@@ -1,6 +1,6 @@
 // Ex 4 Boucle for, Each  & condition
 let leTableau = [4,"true",true,"bonjour",511,"coding school",{},2020,1 ,"maryam",false,[],13,["salut",13,false],"les tartines c'est délicieux","true et false",1070,478,"hello","top",4000,99,"codeur",45,"javascript",399,"autodidacte","oui",1000,1001,"brainstorming","lol"];
-/*
+
 //1
 console.log("\n 1.");
 
